2d903c3c18d0f6dc53ac5209b2b794e6508c6816
dirgawardana <dirgawardana334@gmail.com>
2022-01-18 10:30:04 +0700

    bikin CRUD

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('lombas','LombaController');
+Route::resource('/lombas','LombaController');
